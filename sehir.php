9ffb24e5149c7a357ba45d693e9676f7507db841
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:56 +0300

    Create sehir.php

---
+++ sehir.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style6.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body style="background:rgb(235, 235, 235);">
+<?php
+    include "header.php"
+?>
+    <div class="counter-up6">
+    <div class="content6">
+        <div class="box6">
+            <div class="counter15">Şehir Analiz Sistemleri</div><br>
+            <div class="counter16">Şehir içi güvenliği, Verimlilik ve Zekayı yepyeni bir düzeye taşımak için açık, yenilikçi ve profesyonel Güvenli Şehir çözümleri</div>
+        </div>
+    </div>
+    </div>
+    <!---------------------------------------->
+
+    <?php
+    include "sehiranaliz.php"
+    ?>
+
+
+
+    <!--
+    <div class="section">
+        <div class="container1">
+            <div class="content-section">
+                <div class="content">
+                    <h5>Şehirlerin gelişip büyümesi için güvenli ve huzurlu bir ortam yaratmak çok önemlidir.
+                        Şehir yoğunluklarının artması nedeniyle şehirlerin insanları ve mülkleri korumak 
+                        için daha gelişmiş teknolojilere ihtiyacı vardır. Geçmiş yıllarda şehir yönetimi 
+                        güvenliği sağlamak için tamamen insan gücüne, yani polis ve özel güvenlik görevlilerine 
+                        dayalıydı. Video teknolojilerinin ortaya çıkmasıyla şehirler, olayları yakından takip 
+                        edebilmek için çok daha verimli olan bu yolu tercih etti. Artık en temel gözetim sistemleri 
+                        ilk müdahale ve suç araştırmalarını çok daha kolay ve hızlı hale getiriyor.</h5>
+                      
+                </div>
+            </div>
+            <div class="image-section">
+                <img src="images/hizmetler/akillisehir.png">
+            </div>
+            
+        </div>
+    </div>
+-->
+
+
+
+<?php
+    include "footer.php"
+?>
+</body>
+</html>
