b3da018b9517ddc545e5dad324d1a43e5d24f0a5
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:47 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <meta name="description" content="viva bilgisayar, teknoloji firması, hatay bilgisayar şirketleri, hatay teknoloji, güvenlik kameraları, hikvision hatay, 
+    web tasarımı, toplu sms, hosting, domain">
+    <meta name="keyword" content="HTML, CSS, JQuery, PHP, Web, Tasarım, Site tasarımı, Web sitesi oluştur, hosting, domain">
+    <link rel="stylesheet" href="style.css">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js"></script>
+    
+
+</head>
+<body>
+    <section class="nav2">
+        <div class="container">
+            <div class="row top-bar">
+                <div class="col1">
+                    <span class="text-white">
+                    <a href="hakkimizda.php"><i class="far fa-building"></i>Kurumsal </a>
+                    </span>
+                    <span class="text-white">
+                    <a href="iletisim.php"><i class="fas fa-info-circle"></i>Bize Ulaşın </a>
+                    </span>
+                    <span class="text-white">
+                    <a href="iletisim.php"><i class="fas fa-phone-square-alt"></i>Destek Hattı </a>
+                    </span>
+                </div>
+                <div class="col text-right">
+                    <button type="button" class="btn btn-danger">E-Hizmetler</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php
+        include "header.php"
+    ?>
+
+     <!--------------------------------SLİDESHOW-------------------------------------------------------------------------------------------->
+     <div id="demo" class="carousel slide" data-ride="carousel" data-interval="3000">
+
+    <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1" ></li>            
+        <li data-target="#demo" data-slide-to="2" ></li>
+        <li data-target="#demo" data-slide-to="3" ></li>
+        <li data-target="#demo" data-slide-to="4" ></li>
+    </ul>
+
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img id="cimg" src="images/kameralar.png" alt="" >
+        </div>
+        <div class="carousel-item " >
+            <img id="cimg" src="images/hosting.jpg" alt="" >
+        </div>
+        <div class="carousel-item ">
+            <img id="cimg" src="images/sms.jpg" alt="" >
+        </div>
+        <div class="carousel-item " >
+            <img id="cimg" src="images/webtasarimi.jpg" alt="" >
+        </div>
+        <div class="carousel-item ">
+            <img id="cimg" src="images/bilisim.jpg" alt="" >
+        </div>
+        <div class="carousel-item ">
+            <img id="cimg" src="images/teknik.jpg" alt="" >
+        </div>
+    </div> 
+
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-contol-prev-icon"></span>
+    </a> 
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-contol-next-icon"></span>
+    </a>    
+    </div>
+    <!------------------------------------------------------------Buttons----------------------------------------------------------->
+
+        <div class="btn-group btn-group-1" id="button-group">
+        <button type="button" class="btn" id="button1"><i class="material-icons"  id="icon">domain</i><br><bold>BAYİLİK BAŞVURUSU</bold><br><br><small>Yetkili bayilerimiz arasına siz de katılın.</small></button>
+        <button type="button" class="btn" id="button2"><i class="material-icons"   id="icon">sell</i><br>MARKALARIMIZ<br><br><small>Güçlü markalar ile size hizmet vermekteyiz.</small></button>
+        <button type="button" class="btn" id="button3"><i class="material-icons"  id="icon">notifications</i><br>DUYURULAR<br><br><small>Gelişme ve kampanyalardan haberdar olun.</small></button>
+        </div>
+    <!------------------------------------------------------------------------------------------------------------------------------>
+
+    <!--------------------------------------------------------Explanation----BOX--------------------------------------------------------------->
+    <div class="body8">
+    <div class="about-section1">
+        <div class="inner-container1">
+            
+            <p class="text">
+            <h1>
+                Teknoloji Sektörünün Öncüsüyüz
+                <div class="border"></div>
+            </h1>
+            Viva Bilgisayar, yenilikçi güvenlik kamerası ürün ve çözümlerinin dünya lideri tedarikçisidir. 
+            Sektörün en büyük Ar & Ge gücüne sahip olan Hikvision, ses ve görüntü kodlama, video görüntüsü işleme 
+            ve veri depolama gibi kilit teknolojilerin yanı sıra bulut bilişim,
+            büyük veri ve derin öğrenme gibi geleceğin parlak teknolojilerinde de ilerlemeye devam etmektedir. 
+            Güvenlik kamerası sektörünün yanı sıra akıllı ev teknolojileri, endüstriyel otomasyon ve otomotiv 
+            elektroniği sektörlerine de uzanan Hikvision, uzun-vadeli vizyonunu karşılamayı hedefliyor. 
+            
+            </p>
+        </div>
+    </div>
+    </div>
+    <div class="body9">
+    <div class="about-section2">
+        <div class="inner-container2">
+            
+            <p class="text2">
+            <h1>
+                Bilgisayar ve Güvenlik Teknolojileri
+                <div class="border"></div>
+            </h1>
+            2002'den beri bilişim ve teknoloji alanında faaliyet gösteren Viva Bilgisayar , geçmişten günümüze hızla gelişen
+            çağdaş teknolojilere adapte olarak ve portföyüne yeni servisler ekleyerek müşterilerine hizmet vermeye devam etmektedir.
+            </p>
+            <a href="hakkimizda.php"><button>Daha Fazlası</button></a>
+        </div>
+    </div>
+    </div>
+    <!------------------------------------------------------------------------------------------------------------------------------>
+    <br><br><br>
+    <!--------------------------------------------FLİP-BOX-------------------------------------------------------------------------->
+    <div class="row" id="flip-content">
+    <p><h4>Kurumsal Web Tasarım Hizmeti Verdiğimiz Firmalar</h4><br>
+    19 yılı aşkın bir deneyim ile başta Hatay olmak üzere birçok şehirde 
+    web tasarım alanında birbirinden farklı ve güzel web projeleri gerçekleştirdik. 
+    Bu zamana kadar gerçekleştirmiş olduğumuz web sitesi tasarımlarından 
+    küçük bir kısmını burada sizinle de paylaşmak istedik. 
+    </p><br>
+    </div>
+    <div class="row" id="flip-row">
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img  id="flip-img" src="images/referans/DefneBelediyesi.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Defne Belediyesi</h2>
+                <a href="https://www.defne.bel.tr" target="_blank"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/TopluSMS.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Hatay Toplu SMS</h2>
+                <a href="https://www.hataytoplusms.com" target="_blank"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/HatayHost.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Hatay Host Hizmeti</h2>
+                <a href="https://www.hatayhost.com" target="_blank"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/ZenitHome.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Zenit Home</h2>
+                <a href="https://www.zenithomedekorasyon.com" target="_blank"><br>Link</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/OvaBoru.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Ova Boru</h2>
+                <a href="https://www.ovaboru.com" target="_blank"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/AkkusMarket.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Akkuş Market</h2>
+                <a href="https://www.akkusmarket.com" target="_blank"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/1001BalonFestivali.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>1001 Balon Festivali</h2>
+                <a href="https://www.1001balonfestivali.com" target="_blank"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/FarukYesiloglu.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Faruk Yeşiloğlu</h2>
+                <a href="https://www.farukyesiloglu.com" target="_blank"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <a href="referanslar.php" id="button-flip"><button>Daha Fazlasını Gör</button></a>
+    <br><br><br><br>
+    <!----------------------------------------------------------------------------------------------->
+     <!------------------------------------------------------Section Counter--------------------------------------------------------->
+     <div class="counter-up">
+        <div class="content">
+            <div class="box">
+            <div class="icon"><i class="fa fa-user" style="font-size:50px"></i></div>
+            <div class="counter">33+</div>
+            <div class="textbox"> Ortak</div>
+            </div>
+            <div class="box">
+            <div class="icon"><i class="fa fa-check" style="font-size:50px"></i></div>
+            <div class="counter">1000+</div>
+            <div class="textbox"> Proje</div>
+            </div>
+            <div class="box">
+            <div class="icon"><i class="fa fa-smile-o" style="font-size:50px"></i></div>
+            <div class="counter">1000+</div>
+            <div class="textbox"> Mutlu Kullanıcı</div>
+            </div>
+            <div class="box">
+            <div class="icon"><i class="fa fa-coffee" style="font-size:50px"></i></div>
+            <div class="counter">1000+</div>
+            <div class="textbox"> Toplantı</div>
+            </div>
+        </div>
+    </div>
+    <script>
+        $(document).ready(function(){
+            $(".counter").counterUp({
+                delay: 10,
+                time: 1200
+            });
+        });
+    </script>
+
+ <!------------------------------------------------------------------------------------------------------------------------------>
+    <?php
+        include "footer.php"
+    ?>
+
+
+    
+</body>
+</html>
