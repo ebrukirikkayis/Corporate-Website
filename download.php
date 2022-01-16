3d6888356f2c960f9ecc91458c9c2eeff9417fd7
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:05 +0300

    Create download.php

---
+++ download.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style4.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body style="background:rgb(235, 235, 235)">
+<?php
+    include "header.php"
+?>
+    <div class="container2">
+        <h2>İndirmeler</h2>
+        <p>İşinize yarıyabilecek tüm uygulamaları listede görebilirsiniz:</p>                                                                                      
+        
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>Uygulama</th>
+                        <th>Link</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>1</td>
+                        <td>SADP v3.0.2.4</td>
+                        <td><a href="http://www.hikvisioneurope.com/portal/portal/Technical%20Materials/11%20%20Software/00%20%20Software%20Tool%20Package/01%20%20%20SADP%20Tools/V3.0.2.4/SADPTool.exe" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>2</td>
+                        <td>SADP V1.0.0.3 (for Mac OS 64-bit)</td>
+                        <td><a href="---" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>3</td>
+                        <td>VSPlayer V7.4.0</td>
+                        <td><a href="http://www.hikvisioneurope.com/portal/portal/Technical%20Materials/11%20%20Software/00%20%20Software%20Tool%20Package/00%20%20%20File%20Player/Windows%20VSPlayer/VSPlayer%20V7.4.0/VSPlayer%20V7.4.0.zip" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>4</td>
+                        <td>Batch Configuration Tool v3.0.3.8</td>
+                        <td><a href="http://www.hikvisioneurope.com/portal/portal/Technical%20Materials/11%20%20Software/00%20%20Software%20Tool%20Package/11%20%20%20Batch%20Config%20Tool/V3.0.3.8/Batch%20Configuration.exe" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>5</td>
+                        <td>Hikvision iVMS-4200 v2.8.2.2 (Windows)</td>
+                        <td><a href="https://oversea-download.hikvision.com//Uploadfile/Soft/Client%20Software/iVMS-4200(v2.8.2.2).rar" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>6</td>
+                        <td>Hikvision İVMS-4200 V3.2.0.10 ( Multilingual )</td>
+                        <td><a href="http://www.hikvisioneurope.com/portal/?dir=portal/Technical%20Materials/11%20%20Software/01%20%20Video%20Management%20Software/00%20%20%20iVMS4200/Hikvision%20iVMS-4200%20V3.2.0.10%28Multilingual%29/Normal" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>7</td>
+                        <td>ZKBiosecurity V5000 2.0TR</td>
+                        <td><a href="https://drive.google.com/file/d/1hwrG4OMicQoBVunnuScebSeqMRrHD0Mb/view?usp=sharing" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>8</td>
+                        <td>Biotime Windows x64 8.0.4</td>
+                        <td><a href="https://drive.google.com/file/d/1CuqiQLrkqwMEonRhcHDGtgGVm1QSQaCx/view?usp=sharing" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>9</td>
+                        <td>Team Viewer</td>
+                        <td><a href="https://download.teamviewer.com/full" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>10</td>
+                        <td>ALPEMİX</td>
+                        <td><a href="https://www.alpemix.com/site/Alpemix.exe" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>11</td>
+                        <td>Express Satış</td>
+                        <td><a href="http://nethesap.com.tr/dwnl.php?program=express" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>12</td>
+                        <td>Ticari Program</td>
+                        <td><a href="http://nethesap.com.tr/dwnl.php?program=ticari" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>13</td>
+                        <td>Mağaza Programı</td>
+                        <td><a href="http://nethesap.com.tr/dwnl.php?program=magaza" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>14</td>
+                        <td>Market Programı</td>
+                        <td><a href="http://nethesap.com.tr/dwnl.php?program=market" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>15</td>
+                        <td>FIREBIRD</td>
+                        <td><a href="http://nethesap.com.tr/firebird.exe" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>16</td>
+                        <td>Müşteri Takip Programı</td>
+                        <td><a href="http://nethesap.com.tr/musteritakip.exe" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                        <td>17</td>
+                        <td>CMS</td>
+                        <td><a href="http://www.nvsip.com/files/control/CMS2.0.0.51.rar" download>Download Link</a></td>
+                        </tr>
+                    </tbody>
+
+            </table>
+
+        </div>
+    </div>
+
+</body>
+</html>
+<?php
+    include "footer.php"
+?>
