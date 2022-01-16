d11db6e3e32ff7d083103c6742bed1c8f4fe417e
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:44 +0300

    Create iletsm.php

---
+++ iletsm.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style4.css">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+ 
+
+</head>
+
+<body>
+    <div class="contact">
+        <div class="content">
+            <h2>Bize Ulaşın</h2>
+            <p>----------------</p>
+        </div>
+        <div class="container">
+            <div class="contactInfo">
+                <div class="box">
+                    <div class="icon"><i class="fas fa-map-marker-alt"></i></div>
+                    <div class="text-contact">
+                    
+                    <p><h3>Adres</h3>Odabaşı mah.<br> 31001 Antakya/Hatay</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="icon"><i class="fas fa-phone"></i></div>
+                    <div class="text-contact">
+                    
+                    <p><h3>Telefon</h3>+90(544) 649 11 42<br> +90(552) 528 11 42</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="icon"><i class="fas fa-envelope-open-text"></i></div>
+                    <div class="text-contact">
+                    
+                    <p><h3>Email</h3>yazilim@vivabilgisayar.net</p>
+                    </div>
+                </div>
+            </div>
+         
+    </div>
+</div>
+</body>
+</html>
