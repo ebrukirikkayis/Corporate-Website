ff8f99f4e189d981635c585077e70803d55cb80d
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:53 +0300

    Create referanslar.php

---
+++ referanslar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style5.css">
+</head>
+<body style="background:#ffffff;">
+    <?php
+        include "header.php"
+    ?>
+    <?php
+        include "referanslar-guncel.php"
+    ?>
+    <?php
+        include "footer.php"
+    ?>
+
+</body>
+</html>
