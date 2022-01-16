2c944e5edbda87b9aac52038f79b49c974d919da
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:41 +0300

    Create iletisim.php

---
+++ iletisim.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style4.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+ 
+
+</head>
+<?php
+    include "header.php"
+?>
+<body>
+<?php
+    include "iletsm.php"
+?>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3218.215859300795!2d36.16700194796591!3d36.234249988122734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1525c34f229a597d%3A0x9c06247c8700c7ab!2sHaikon%20Hikvision%20Hatay%20Dealer%20Partner%20Viva%20Bilgisayar%20ve%20G%C3%BCvenlik%20Sistemleri!5e0!3m2!1str!2str!4v1627564953633!5m2!1str!2str" 
+    width="100%" height="450" style="border-top:1px solid #D9151D ; padding-top:0px;" allowfullscreen="" loading="lazy"></iframe>
+
+</body>
+</html>
+<?php
+        include "footer.php"
+?>
