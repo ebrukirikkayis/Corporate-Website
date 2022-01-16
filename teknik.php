6f7c845f31806566952bb00c7c21ee37121a39e4
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:20:30 +0300

    Create teknik.php

---
+++ teknik.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style7.css">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+    <div class="container-teknik">
+        <p id="span-teknik2">Elektronik cihazınızı güvenle teslim edebileceğiniz firmamız dahilinde teknik servisimiz 
+            bulunmaktadır. Viva Bilgisayar teknolojik alanda gösterdiği faaliyeti hem yazılım hem donanım kapasitesiyle 
+            desteklemektedir.Bilgisayar tamiri, kamera tamiri, kamera sistemi kurulumu,
+                        alarm sistemi kurulumu, acil durum sistemleri bakım ve onarımı firmamız tarafından gerçekleştirilir. </p>
+        <div class="col-span-12" id="teknik-head">
+            <p id="span-teknik">Tamir</p>
+        </div>
+        <img id="teknik-img" src="images/teknikservis/bg1.png">
+        <div class="col-span-12" id="teknik-head">
+            <p id="span-teknik">Yedek Parça</p>
+        </div>
+        <img id="teknik-img" src="images/teknikservis/bg2.png">
+        <div class="col-span-12" id="teknik-head">
+            <p id="span-teknik">2.El Cihaz</p>
+        </div>
+        <img id="teknik-img" src="images/teknikservis/bg3.png">
+
+
+
+    </div>
+
+
+</body>
+</html>
