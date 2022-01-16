f28e50314aa9e2f9a76cd2856930eff3a9ebca4f
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:20:00 +0300

    Create sehiranaliz.php

---
+++ sehiranaliz.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style7.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container-sehir">
+        <div class="col-span-12" id="colhead">
+            <p id="span-id">GENEL BAKIŞ</p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id1">Şehirlerin gelişip büyümesi için güvenli ve huzurlu bir ortam yaratmak çok önemlidir. 
+                Şehir yoğunluklarının artması nedeniyle şehirlerin insanları ve mülkleri korumak için daha gelişmiş 
+                teknolojilere ihtiyacı vardır. Geçmiş yıllarda şehir yönetimi güvenliği sağlamak için tamamen insan 
+                gücüne, yani polis ve özel güvenlik görevlilerine dayalıydı. Video teknolojilerinin ortaya çıkmasıyla 
+                şehirler, olayları yakından takip edebilmek için çok daha verimli olan bu yolu tercih etti. Artık en 
+                temel gözetim sistemleri ilk müdahale ve suç araştırmalarını çok daha kolay ve hızlı hale getiriyor.</p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id1">Bu elbette daha verimli ve akıllı şehir yönetiminin yalnızca ilk adımıdır. Aşağıdaki grafikte 
+                güvenli bir şehrin genel gelişimi gösterilmektedir. Yalnızca akıllı uygulamalar değil, nihayetinde bu süreçten 
+                elde edilen bilgiler yeni, şaşırtıcı uygulamalar yaratarak güvenli şehirlerin sürekli gelişmesini sağlayabilir.</p>
+        </div>
+        <br><br>
+        <div class="col-span-12" id="colhead">
+            <p id="span-id">SUNDUĞUMUZ HİZMETLER</p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id1">Yukarıda belirtilen eylem planının ardından, tüm Güvenli Şehir gelişimlerinin çok aşamalı bir süreç 
+                olacağı kolaylıkla görülebilir. Yeni bir aşamadaki her adım yeni olanaklar ve fırsatlar getirir. Aşağıdaki grafik, 
+                Hikvision tarafından bu aşamalar için sağlanan tüm kontrol sistemlerini göstermektedir.</p>
+        </div>
+        <br>
+        <div class="col-span-12" id="colcontent">
+        <img src="images/hizmetler/akillisehirhiz.png" id="colcontent-img">
+        </div>
+        <br>
+        <div class="col-span-12" id="colheadsec">
+            <p id="span-id2"><b>1. Aşama</b>: Büyümeye devam edecek bir kentsel güvenlik sisteminin temelini atın</p>
+        </div>
+        <br>
+        <div class="col-span-12" id="colcontent">
+        <img src="images/hizmetler/akillisehirhiz2.png" id="colcontent-img">
+        </div>
+        
+        <div class="col-span-12" id="colheadsec">
+            <p id="span-id3">Yer Kontrol Sistemi</p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">Hikvision, kamu güvenliği ihtiyaçlarını karşılamak üzere karmaşık şehir senaryoları için tasarlanmış 
+                güvenlik kameraları sunar. Yer Kontrol Sistemi, şehir güvenliği altyapısının temelini oluşturur. Başlıca lokasyonlarda 
+                ve şehrin anayollarında bulunan kameralar, karar mercileri için sürekli olarak değerli bilgiler sağlar.</p>
+        </div>
+        <div class="col-span-12" id="colheadsec">
+            <p id="span-id3">Hava Kontrol Sistemi</p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">Yüksek zum oranına sahip PTZ'ler, konumlandırma sistemleri, PanoVu kameralar, insansız hava araçları ve anti-UAV 
+                ekipmanları şehrin yüksek noktalarına yerleştirilerek bir hava alanı koruma sistemi oluşturulur. Dinamik ve statik düğümlerin bir 
+                arada kullanılmasının yanı sıra küresel algı ve ayrıntı yakalama kombinasyonu, Hava Kontrol sistemini şehir güvenliğinin ayrılmaz 
+                bir parçası haline getirir.</p>
+        </div>
+        <div class="col-span-12" id="colheadsec">
+            <p id="span-id3">Mobil Kontrol Sistemi</p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">Sabit izleme genellikle önemli şehir alanlarını kapsar ancak kapsam sınırlıdır. Şehir genelinde bu, hala bazı kör noktalar 
+                olacağı anlamına gelir. Mobil Kontrol Sistemi bu sorunu çözmek için tasarlanmıştır. Sistem, esnek ve çevik tepkiler için gövdeye monte edilen 
+                kameralar ve taşınabilir PTZ dome'lardan oluşur, acil durumlarda ve sabit izlemenin mümkün veya kolay olmadığı durumlarda hızlı devreye 
+                almayı sağlar.</p>
+        </div>
+
+        <div class="col-span-12" id="colheadsec">
+            <p id="span-id3">Alarm Kontrol Sistemi</p>
+        </div>
+
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">Alarm Kontrol Sistemi kurulması insan kaynaklarını büyük ölçüde azaltır ancak bunun kesin doğruluğa göre yapılması gerekir. 
+                Hikvision'ın Akıllı Algılama Teknolojisi, komut merkezi tarafından alınan alarmların geçerli ve yanıt vermeye değer olmasını sağlar. Aynı 
+                zamanda, polis memurlarının bir olay meydana gelmeden önce risk altındaki ve tehdit altındaki kişilerle görüntülü arama yapabilmeleri nedeniyle, 
+                şehirde acil durum alarmlarının uygulanması sivillerin güven duygusunu da artıracaktır.</p>
+        </div>
+
+        <div class="col-span-12" id="colheadsec">
+            <p id="span-id2"><b>2. Aşama</b>: Güvenli Şehrin akıllılık seviyesini yükselten yeni bir kontrol sistemi tanıtın</p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+        <img src="images/hizmetler/akillisehirhiz5.png" id="colcontent-img">
+        </div>
+
+        <div class="col-span-12" id="colheadsec">
+            <p id="span-id3">Akıllı Kontrol Sistemi</p>
+        </div>
+        
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">AI teknolojileri geliştikçe şehir güvenliği, verimlilikte eşi benzeri görülmemiş iyileştirmelerle heyecan verici, yeni bir akıllı çağa 
+                giriyor. Geniş yapay zeka özellikleri ve video tabanlı güvenlik endüstrisinin derin anlayışıyla Hikvision, özel derin öğrenme algoritmalarını uç etki 
+                alanlarındaki akıllı algılama cihazlarına ve bulut içindeki güçlü bilgi işlem cihazlarına entegre eder. Sonuç olarak bu, etkili ve hassas veriler üretir 
+                ve bu verilere dayalı olarak daha da işlevsel güvenlik uygulamaları sağlar.</p>
+        </div>
+        <div class="col-span-12" id="colheadsec">
+            <p id="span-id3">Veri Merkezi</p>
+        </div>
+
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">Beş kontrol sistemi katmanı, bir şehrin etraflı bir şekilde kapsanmasını sağlamak için bir araya gelir ancak bu yeterli değildir. Eksiksiz bir 
+                Güvenli Şehir için merkezi bir sistem inşa edilmesi kaçınılmaz bir gerekliliktir. Bu, şehirde binlerce cihazı birbirine bağlayan bir beyin olacaktır. Hikvision; 
+                görselleştirilmiş, gerçek zamanlı ve akıllı bir merkezi sistem oluşturmak için bozulmaya dayanıklı video depolama çözümleri, yüksek tanımlı geniş ekranlar ve merkezi 
+                bilgi işlem, büyük veri ve AI algoritması eğitim tesislerine aktarımdan oluşan bir dizi merkezi donanım sunar. Ayrıca Hikvision, veri işleme ve analizlerine dayanan zengin, 
+                çok boyutlu uygulama özelliklerini sağlamak ve paylaşmak için güçlü bir uygulama sistemi de sunar.</p>
+        </div>
+
+        <div class="col-span-12" id="colheadsec">
+            <p id="span-id2"><b>3. Aşama</b>: Dünyanın dört bir yanındaki iş ortakları ve son kullanıcılarla bir başlangıç</p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+        <img src="images/hizmetler/akillisehirhiz3.png" id="colcontent-img">
+        </div>
+        <br>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">Birinci aşamada şehre yerleştirilen çok sayıda video kaynağıyla zemin çalışmaları gerçekleştirilir, böylece ikinci aşamadaki akıllı yükseltmelerle bol miktarda 
+                veri oluşturulur. Bu, müşterilerin iş bilgileriyle bir araya gelerek yerel kentsel güvenlik sistemlerinin ve hizmetlerinin daha iyi anlaşılmasına ve sağlanmasına yardımcı 
+                olacak olan şehrin Nesnelerin İnterneti Verileri haline gelir.</p>
+        </div>
+        <br><br>
+        <div class="col-span-12" id="colhead">
+            <p id="span-id">HIKVISION AI BULUTU</p>
+        </div>
+
+        <div class="col-span-12" id="colcontent">
+        <img src="images/hizmetler/akillisehirhiz4.png" id="colcontent-img2">
+        </div>
+        <br>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">Yukarıdaki üç aşama boyunca sistem geliştirme çalışmaları; Uç Düğümleri, Uç Etki Alanları ve Bulut Merkezi'nin, her seviyede ve çeşitli kuruluşlarda gereksinimleri 
+                karşılayarak Güvenli Şehir Çözümü'nün çeşitli senaryolarına uyum sağlamak üzere tahsis edildiği AI Bulut mimarisine dayalı olacaktır.</p>
+        </div>
+        <div class="col-span-12" id="colheadthr">
+            <p id="span-id3"><b>Bulut Merkezi: Kapsamlı Uygulamalar</b></p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">İş verilerini birleştirme ve çok boyutlu büyük veri analizi uygulaması</p>
+        </div>
+        <div class="col-span-12" id="colheadthr">
+            <p id="span-id3"><b>Uç Etki Alanı: Sahneler Arası Uygulamalar</b></p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">Akıllı uygulamalar ve verilerin toplanması, depolanması ve işlenmesi</p>
+        </div>
+        <div class="col-span-12" id="colheadthr">
+            <p id="span-id3"><b>Uç Düğümü: Tek Sahne Uygulamaları</b></p>
+        </div>
+        <div class="col-span-12" id="colcontent">
+            <p id="span-id4">Çok boyutlu sensörlerden ve ön uç işlemeden veri toplama</p>
+        </div>
+        <br>
+        <br>
+
+    </div>
+    
+</body>
+</html>
