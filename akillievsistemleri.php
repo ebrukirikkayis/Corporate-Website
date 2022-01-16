5a57fe23b7e18a19ae6069fa48b08839eac965e5
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:18:57 +0300

    Create akillievsistemleri.php

---
+++ akillievsistemleri.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style7.css">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <!--<div class="col-span-12" id="colheader">
+        <p id="span-sp">KABLOSUZ AKILLI EV VE GÜVENLİK SİSTEMİ</p>
+    </div> -->
+    <div class="col-span-12" id="colheader">
+        <p id="span-sp1">DONANIMSAL ÖZELLİKLER</p>
+        <p id="span-sp2">Şık ve zarif ürün tasarımları ile evinizin baş köşesine koyabilirsiniz</p>
+    </div>
+
+    <div class="container-home" id="donanimsal">
+        <div class="col-span-4">
+            <p id="span-sp3"><i class="fas fa-check-double"></i><b> Stabil Çalışma</b></p>
+            <p id="span-sp4">Defalarca testten geçmiş altyapısı ile gönül rahatlığı kullanabileceğiniz 
+                entegre Akıllı Ev ve Güvenlik Sistemidir.
+            </p>
+
+            <p id="span-sp3"><i class="fas fa-home"></i><b> Akıllı Otomasyon Sistemi</b></p>
+            <p id="span-sp4">IP kameralar, akıllı priz, panik butonu, gaz, duman, su ve titreşim sensörleri 
+                ile sadece bir güvenlik sistemi değil, aynı zamanda Kablosuz Akıllı Ev Sistemi’dir.
+            </p>
+
+            <p id="span-sp3"><i class="fas fa-mobile-alt"></i><b> Hızlı ve Kolay Kurulum</b></p>
+            <p id="span-sp4">Cep telefonunuzdan aksesuarlardaki QR kodları 
+                okutarak 5 dk içinde aksesuarları kolayca ekleyebilirsiniz.
+            </p>
+
+            <p id="span-sp3"><i class="fas fa-ruler-vertical"></i><b> Geniş İletişim Menzil</b></p>
+            <p id="span-sp4">200 Mt. menzil ile birçok alanda kullanım sağlar. 
+                Sinyal yükselticilere olan ihtiyacı ortadan kaldırır.
+            </p>
+        </div>
+
+
+        <div class="col-span-4">
+            <img id="span-img" src="images/hizmetler/akilliev2.png">
+        </div>
+
+        <div class="col-span-4">
+                <p id="span-sp3"><i class="fas fa-wifi"></i><b> Kablosuz İletişim</b></p>
+                <p id="span-sp4">Şarj edilir ve değişebilir piller ile desteklenen aksesuarlar, 
+                    kablosuz olarak ana panele bağlanır. Kablo maliyetini ve kirliliğini ortadan kaldırır.
+                </p>
+
+                <p id="span-sp3"><i class="fas fa-lock"></i><b> Şifreli Güvenlik</b></p>
+                <p id="span-sp4">Uluslararası standartta “Çift yönlü AES-128“ şifreleme sunan sistem, siren veya 
+                    sensörlere müdahale olması durumunda, “Sabotaj” uyarısı göndermektedir.
+                </p>
+
+                <p id="span-sp3"><i class="fas fa-link"></i><b> Alarm Merkezine Bağlanma</b></p>
+                <p id="span-sp4">Gerekirse dilediğiniz haber alma merkezine bağlanma olanağı sunan sistem ile LAN, 
+                    Wi-Fi veya GSM veri bağlantısı sağlayabilirsiniz.
+                </p>
+
+                <p id="span-sp3"><i class="fas fa-project-diagram"></i><b> Sınırsız Aksesuar Desteği</b></p>
+                <p id="span-sp4">Sınırsız kullanıcı ve aksesuar özelliği ile 
+                    dilediğiniz kadar ekleyebilirsiniz.
+                </p>
+        </div>
+    </div>
+    
+    <div class="container-yazilimsal">
+    <div class="col-span-12" id="colheader">
+        <p id="span-sp1">YAZILIMSAL ÖZELLİKLER</p>
+        <p id="span-sp2">Türkçe ve ücretsiz kullanıcı dostu ara yüz ile tüm ayarlarınızı cebinizden kolaylıkla yapın.</p>
+    </div>
+    <div class="container-home" id="yazilimsal">
+        <div class="col-span-4">
+            <p id="span-sp3"><i class="fas fa-microphone"></i><b> Siri (IOS) ile Kontrol</b></p>
+            <p id="span-sp4">Siri (iOS) ile çalışan sistem telefonunuzdan uygulamayı açmadan 
+                Ses komutu ile sistemi devreye alıp çıkarabilirsiniz.
+            </p>
+
+            <p id="span-sp3"><i class="fas fa-home"></i><b> Evde Alarm Modu</b></p>
+            <p id="span-sp4">Evde alarm modunda iken, hangi sensörlerin aktif olacağını, hangi 
+                sensörlerin pasif hale geçeceğini belirleyebilirsiniz.
+            </p>
+        </div>
+
+
+        <div class="col-span-4">
+                <p id="span-sp3"><i class="fas fa-users"></i><b> Kullanıcı Yetkilendirme</b></p>
+                <p id="span-sp4">Sınırsız kullanıcı desteği yanında “Yönetici, Kullanıcı ve 
+                    Misafir” gibi 3 düzeyde kullanıcı yetkilendirilebilir.
+                </p>
+
+                <p id="span-sp3"><i class="fas fa-user-shield"></i><b> Tehdit Alarmı</b></p>
+                <p id="span-sp4">Alarmı sessize alabilir ve gizlice alarm durumunu gönderebilirsiniz, 
+                    SOS Panik butonu ile sessiz alarm gönderebilrisiniz.
+                </p>
+
+        </div>
+
+        <div class="col-span-4">
+                <p id="span-sp3"><i class="fas fa-bell"></i><b> Herkesin Haberi Olsun!</b></p>
+                <p id="span-sp4">Uygulama yüklü olmasa bile, alarm durumunda istenen 
+                    kişilere alarm gönderim olanağı sağlar.
+                </p>
+
+                <p id="span-sp3"><i class="far fa-bell"></i><b> Alarm Kurma Derdi Yok!</b></p>
+                <p id="span-sp4">Belirlediğiniz gün ve saatlerde otomatik alarmı devreye alır ve devre dışı bırakır. 
+                    Siz unutsanız bile eviniz her zaman güvende olur.
+                </p>
+        </div>
+    </div>
+    </div>
+
+
+    <div class="col-span-12" id="colheader">
+        <p class="container-tanitim">TANITIM VİDEOLARI</p>
+    </div>
+
+    <div class="container-home">
+        <div class="col-span-6">
+            <iframe id="span-video" src="https://www.youtube.com/embed/_7VsKIVLxXA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe id="span-video" src="https://www.youtube.com/embed/eQ92TK5OWZ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div class="col-span-6">
+            <iframe id="span-video" src="https://www.youtube.com/embed/T4FisTDbZVU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe id="span-video" src="https://www.youtube.com/embed/fzCgw07omXE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+
+</body>
+</html>
