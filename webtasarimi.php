1722f0e1964d9c9c2a17f220b42d7a2e942d7511
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:20:41 +0300

    Create webtasarimi.php

---
+++ webtasarimi.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style6.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body style="background:rgb(235, 235, 235);">
+<?php
+    include "header.php"
+?>
+    <div class="counter-up2">
+    <div class="content2">
+        <div class="box2">
+            <div class="counter3">Web Tasarımı</div><br>
+            <div class="counter4">Kurumsal Web Tasarım, E-ticaret Sitesi, CRM Sistemleri, Masaüstü Uygulama ve Otomasyon</div>
+        </div>
+    </div>
+    </div>
+    <!---------------------------------------->
+    <div class="section">
+        <div class="container1">
+            <div class="content-section">
+                <div class="content5">
+                    <h5>Viva Bilgisayar 2002 yılından bu yana web ve mobil tabanlı yazılım çözümleri sunan, 
+                        işletmeniz ile ilgili dijital tüm platformlarda özveri ile sizlere hizmet vermeyi ilke edinmiş 
+                        Hatay şehrinin önde gelen kurumsal web tasarım, e-ticaret, crm sistemleri, masaüstü uygulama ve 
+                        otomasyon sağlayıcısıdır. Küçük büyük demeden, her ölçekten firmanın iş hedeflerini gerçekleştirme 
+                        sürecinde rekabet güçlerini artıracak, katma değer oluşturacak, kaliteli projeler ve çözümler 
+                        üreten bir yazılım firmasıdır.</h5>
+                        <div class="button5">
+                            <a href="referanslar.php">Göz At</a>
+                        </div>
+                </div>
+            </div>
+            <div class="image-section">
+                <img src="images/hizmetler/web-tasarim.png">
+            </div>
+            
+        </div>
+    </div>
+
+
+
+<?php
+    include "footer.php"
+?>
+</body>
+</html>
