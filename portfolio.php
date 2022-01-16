edeb1aa2a3187818be2375b9aa23205f4e0cf3b6
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:50 +0300

    Create portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+    <section>
+        <ul>
+            <li class="list active" data-filter="all">Tüm Ürünler</li>
+            <li class="list" data-filter="acu">AcuSense Kameralar</li>
+            <li class="list" data-filter="ipk">Network IP Kayıt Cihazları</li>
+            <li class="list" data-filter="color">ColorVu Kameralar</li>
+            <li class="list" data-filter="analog">Analog HD Kameralar</li>
+            <li class="list" data-filter="hdr">HDR DVR Kayıt Cihazları</li>
+            <li class="list" data-filter="black">Siyah Renkte Kameralar</li>
+        </ul>
+        <div class="product">
+            <div class="itemBox acu"><img src="images/portfolio/1.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CE16D0T-IRF 2MP HD-TVI IR Bullet Kamera</div>
+                </div>
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/2.png">
+                <div class="overlay">
+                    <div class="text-product">Hikvision 2.0MP 4.0mm Lens 30Mt. IR IP Bullet Kamera</div>
+                </div>
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/3.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CD2121G0-IS/2AX LR IP Kamera</div>
+                </div>
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/4.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CE16C2T-IR 720P Turbo HD Night Vision Outdoor Bullet Kamera</div>
+                </div>
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/5.png">
+                <div class="overlay">
+                <div class="text-product">Haikon DS-2CD2020F-I 2.0mp POE IP Güvenlik Kamerası</div>
+                </div>
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/6.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CD2955FWD-I 5 MP Fisheye Fixed Dome Network Kamera</div>
+                </div>
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/7.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CD6520D-IO 2MP IP Mobil Kamera</div>
+                </div>
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/8.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CD2620F-I 2MP Varifocal IR Bullet IP Kamera</div>
+                </div>
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/9.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CE16D0T-ITF 2 MP AHD Bullet Kamera </div>
+                </div>
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/10.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CD2T25FWD-I5 2MP IP IR Bullet Kamera</div>
+                </div>
+            </div>
+            <div class="itemBox color"><img src="images/portfolio/11.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CE70DF3T-PFS Camera</div>
+                </div>  
+            </div>
+            <div class="itemBox ipc"><img src="images/portfolio/12.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision Ds 2CD2621G0 Izs 2mp Motorize Lensli Ip Kamera</div>
+                </div>
+            </div>
+            <div class="itemBox black"><img src="images/portfolio/13.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CD2T85G1-I8 Bullet IP Camera 8MP 4mm (78 °) fixed lens Darkfighter</div>
+                </div>
+            </div>
+            <div class="itemBox black"><img src="images/portfolio/14.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CD2365G1-I 6MP Black Shadow Outdoor Turret CCTV Camera, H.265+, 30m IR ft Darkfighter Technology</div>
+                </div>
+            </div>
+            <div class="itemBox black"><img src="images/portfolio/15.png">
+                <div class="overlay">
+                <div class="text-product">DS-2CD2T45FWD-I8 4MP İP Güvenlik Kamerası</div>
+                </div>
+            </div>
+            <div class="itemBox black"><img src="images/portfolio/16.png">
+                <div class="overlay">
+                <div class="text-product">DS-2XM6612FWD-I Hikvision 1.3MP IP Gece Görüşlü Araç Kamerası</div>
+                </div>
+            </div>
+            <div class="itemBox black"><img src="images/portfolio/17.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision DS-2CD2145FWD-IS 4Mp İp Dome Kamera siyah</div>
+                </div>
+            </div>
+            <div class="itemBox black"><img src="images/portfolio/18.png">
+                <div class="overlay">
+                <div class="text-product">4 MP IP camera Hikvision DS-2CD2043G0-I (2.8 mm)</div>
+                </div>
+            </div>
+            <div class="itemBox black"><img src="images/portfolio/19.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision 6MP Shadow Series Outdoor Motorised VF Dome, 30m IR, WDR, IP67, 2.8-12mm</div>
+                </div>
+            </div>
+            <div class="itemBox black"><img src="images/portfolio/20.png">
+                <div class="overlay">
+                <div class="text-product">Hikvision AE-DN2016-F3 Mobil Full Hd Araç Kamerası</div>
+                </div>
+            </div>
+            <!--
+            <div class="itemBox black"><img src="images/portfolio/21.png">
+                <div class="overlay">
+                <div class="text">Hikvision 6MP Shadow Series Outdoor Mini Dome Camera, 10m IR, WDR, IK8, 2.8mm, Black</div>
+                </div>
+            </div>
+            --->
+        </div>
+    </section>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('.list').click(function(){
+                const value = $(this).attr('data-filter');
+                if(value=='all'){
+                    $('.itemBox').show('1000');
+                }
+                else{
+                    $('.itemBox').not('.'+value).hide('1000');
+                    $('.itemBox').filter('.'+value).show('1000');
+                }
+            })
+            $('.list').click(function () {
+                $(this).addClass('active').siblings().removeClass('active');
+            })
+        })
+    </script>
+
+
+
+
+
+
+
+
+
+    <!--
+    <div class="main">
+
+    <img src="images/viva-logo.png" style="width:250px; height:150px; "alt="">
+    <hr>
+
+    <h2>PORTFOLIO</h2>
+
+    <div id="myBtnContainer">
+    <button class="btn-2 active" onclick="filterSelection('all')"> Tümünü Göster</button>
+    <button class="btn-2" onclick="filterSelection('ip')"> IP Camera</button>
+    <button class="btn-2" onclick="filterSelection('color')"> Color VU</button>
+    <button class="btn-2" onclick="filterSelection('black')"> Siyah Renk</button>
+    </div>
+
+     Portfolio Gallery Grid 
+    <div class="row">
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/1.png" alt="ip" style="width:100%">
+        <h4>Mountains</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/2.png" alt="ip" style="width:100%">
+        <h4>Lights</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/3.png" alt="ip" style="width:100%">
+        <h4>Lights</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/4.png" alt="ip" style="width:100%">
+        <h4>Lights</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/5.png" alt="ip" style="width:100%">
+        <h4>Lights</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/6.png" alt="ip" style="width:100%">
+        <h4>Lights</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/7.png" alt="ip" style="width:100%">
+        <h4>Lights</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/8.png" alt="ip" style="width:100%">
+        <h4>Lights</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/9.png" alt="ip" style="width:100%">
+        <h4>Forest</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/10.png" alt="ip" style="width:100%">
+        <h4>Retro</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column color">
+        <div class="content">
+        <img src="images/portfolio/11.png" alt="color" style="width:100%">
+        <h4>Fast</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column ip">
+        <div class="content">
+        <img src="images/portfolio/12.png" alt="ip" style="width:100%">
+        <h4>Classic</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+
+    <div class="column black">
+        <div class="content">
+        <img src="images/portfolio/13.png" alt="black" style="width:100%">
+        <h4>Girl</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    <div class="column black">
+        <div class="content">
+        <img src="images/portfolio/14.png" alt="black" style="width:100%">
+        <h4>Man</h4>
+        <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    END GRID 
+    </div>
+
+    END MAIN
+    </div>
+
+        <script>
+        filterSelection("all")
+        function filterSelection(c) {
+        var x, i;
+        x = document.getElementsByClassName("column");
+        if (c == "all") c = "";
+        for (i = 0; i < x.length; i++) {
+            w3RemoveClass(x[i], "show");
+            if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+        }
+        }
+
+        function w3AddClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+        }
+        }
+
+        function w3RemoveClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);     
+            }
+        }
+        element.className = arr1.join(" ");
+        }
+
+
+        // Add active class to the current button (highlight it)
+        var btnContainer = document.getElementById("myBtnContainer");
+        var btns = btnContainer.getElementsByClassName("btn-2");
+        for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function(){
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+        }
+        </script>
+
+    -->
+</body>
+</html>
