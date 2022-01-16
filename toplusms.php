4e94f93325fa61ccfd31085859bcc30ab833ea55
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:20:36 +0300

    Create toplusms.php

---
+++ toplusms.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style6.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<?php
+    include "header.php"
+?>
+<div class="counter-up3">
+    <div class="content3">
+        <div class="box3">
+            <div class="counter9">Toplu SMS Hizmeti</div><br>
+            <div class="counter10">VİVA Bilgisayar Markası olan HATAY TOPLU SMS</div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+    include "sms.php"
+?>
+<!--
+<div class="section">
+        <div class="container1">
+            <div class="content-section2">
+                <div class="content5">
+                    <h5> VİVA Bilgisayar Markası olan HATAY TOPLU SMS 2003 yılından bu yana teknoloji alanında faaliyet gösterip, günümüze kadar profesyonel çözümler üretmeye devam ediyor.
+                        Şu an da Toplu SMS, kurumlar için geleneksel iletişim yöntemleriyle karşılaştırıldığında hızlı, ekonomik, etkili ve güvenilir bir iletişim ortamı sağlamaktadır.
+                        Biz HATAY TOPLU SMS ile hizmet kalitemiz ve müşteri memnuniyetimizle ön plana çıkıyoruz, 7/24 kesintisiz hizmet veriyoruz.
+                        Binlerce müşteriye ve yüz milyonlarca SMS trafiğine alt yapı sağlayan firmamız, müşteri memnuniyeti odaklıdır ve işinize değer yaratmak için çalışıyoruz.</h5>
+                        <div class="button5">
+                            <a href="https://hataytoplusms.com/" target="_blank">Göz At</a>
+                        </div>
+                        </div>
+            </div>
+            <div class="image-section2">
+                <img src="images/bg-toplusms-banner.png" >
+            </div>
+            
+        </div>
+    </div>
+
+-->
+
+
+
+
+
+
+
+<?php
+    include "footer.php"
+?>
+</body>
+</html>
