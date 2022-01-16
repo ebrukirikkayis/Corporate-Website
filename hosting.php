b956f7ee205951f3bdd22dc66aed98abb11dd684
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:37 +0300

    Create hosting.php

---
+++ hosting.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style6.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<?php
+    include "header.php"
+?>
+    <div class="counter-up1">
+    <div class="content1">
+        <div class="box1">
+            <div class="counter1">Hosting Paketleri</div><br>
+            <div class="counter2">Kurumsal Hosting'de Tüm Limitleri Kaldırdık!</div>
+        </div>
+    </div>
+    </div>
+
+    <!--------Pricing Table-------->
+    <div class="body1" style="background:rgb(235, 235, 235); display: flex; align-items: center;">
+    <div class="pricing-table">
+        <div class="pricing-card">
+            <h3 class="pricing-card-header">Tek Site</h3>
+            
+                <div class="price"><sup>TL</sup> 99<span> /yıl</span></div>
+                <ul>
+                    <li><strong>1</strong> Website</li>
+                    <li><strong>Sınırsız</strong> E-posta</li>
+                    <li><strong>Sınırsız</strong> Disk Alanı</li>
+                    <li><strong>Sınırsız</strong> Trafik</li>
+                    <li><strong>Ücretsiz</strong> SSL Sertifikası</li>
+                </ul>
+                <a href="iletisim.php" class="order-btn">Sipariş Ver</a>
+            
+        </div>
+
+        <div class="pricing-card">
+            <h3 class="pricing-card-header">10 Site</h3>
+            
+                <div class="price"><sup>TL</sup> 199<span> /yıl</span></div>
+                <ul>
+                    <li><strong>10</strong> Website</li>
+                    <li><strong>Sınırsız</strong> E-posta</li>
+                    <li><strong>Sınırsız</strong> Disk Alanı</li>
+                    <li><strong>Sınırsız</strong> Trafik</li>
+                    <li><strong>Ücretsiz</strong> SSL Sertifikası</li>
+                </ul>
+                <a href="iletisim.php" class="order-btn">Sipariş Ver</a>
+            
+        </div>
+
+        <div class="pricing-card">
+            <h3 class="pricing-card-header">Sınırsız Site</h3>
+            
+                <div class="price"><sup>TL</sup> 349<span> /yıl</span></div>
+                <ul>
+                    <li><strong>Sınırsız</strong> Website</li>
+                    <li><strong>Sınırsız</strong> E-posta</li>
+                    <li><strong>Sınırsız</strong> Disk Alanı</li>
+                    <li><strong>Sınırsız</strong> Trafik</li>
+                    <li><strong>Ücretsiz</strong> SSL Sertifikası</li>
+                </ul>
+                <a href="iletisim.php" class="order-btn">Sipariş Ver</a>
+            
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php
+    include "footer.php"
+?>
+</body>
+</html>
