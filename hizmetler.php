6d47bbdd0ce5654018351f201138fba890e3b158
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:34 +0300

    Create hizmetler.php

---
+++ hizmetler.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style6.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<?php
+    include "header.php"
+?>
+
+    <div class="wrapper">
+        <h1>Hizmetlerimiz</h1>
+        
+
+        <div class="content-box">
+        <div class="card">
+                <i class="bx bx-globe bx-md"></i><a href="hosting.php">
+                <h2>Hosting Hizmetleri</h2>
+                <p>.....................</p>
+                </a>
+            </div>
+            <div class="card">
+                <i class="bx bx-laptop bx-md"></i><a href="webtasarimi.php">
+                <h2>Web Tasarımı</h2>
+                <p>.....................</p></a>
+            </div>
+           <div class="card">
+                <i class="bx bx-mail-send bx-md"></i><a href="toplusms.php">
+                <h2>Toplu SMS</h2>
+                <p>.....................</p></a>
+            </div>
+            <div class="card">
+                <i class="bx bx-cog bx-md"></i><a href="teknikservis.php">
+                <h2>Teknik Servis</h2>
+                <p>.....................</p></a>
+            </div>
+            <div class="card">
+                <i class="bx bx-home-circle bx-md"></i><a href="akilliev.php">
+                <h2>Akıllı Ev Sistemleri</h2>
+                <p>.....................</p></a>
+            </div>
+            <div class="card">
+                <i class="bx bx-buildings bx-md"></i><a href="sehir.php">
+                <h2>Şehir Analiz Sistemleri</h2>
+                <p>.....................</p></a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+<?php
+    include "footer.php"
+?>
+</body>
+</html>
