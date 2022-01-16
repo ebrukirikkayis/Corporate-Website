0ae6e0b117088b10a9a6182e802f9dcf8179be74
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:18 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<footer >
+    <div class="footerbody">
+        <div class="container-footer">
+            <div class="sec aboutus">
+                <h2>Hakkımızda</h2>
+                <p>2002'den beri Bilişim ve Teknoloji alanında faaliyet gösteriyoruz. Geçmişten günümüze hızla gelişen çağdaş
+                    teknolojilere adapte olarak müşterilerimize hizmet
+                    vermekteyiz.</p>
+                <ul class="sci">
+                
+                <li><a href="https://www.instagram.com/vivabilgisayar/" target="_blank"><i class="material-icons" >link</i></a></li>
+                <li><a href="https://haikonhatay.com/" target="_blank"><i class="material-icons" >link</i></a></li>
+                <li><a href="#"><i class="material-icons" >link</i></a></li>
+            </ul>
+            </div>
+            <div class="sec quickLinks">
+                <h2>Linkler</h2>
+                <ul id="sec-footer-ul">
+                    <li><a href="hakkimizda.php">> Hakkımızda</a></li>
+                    <li><a href="hizmetler.php">> Hizmetler</a></li>
+                    <li><a href="urunler.php">> Ürünler</a></li>
+                    <li><a href="download.php">> İndirmeler</a></li>
+                    <li><a href="iletisim.php">> İletişim</a></li>
+                    <li><a href="referanslar.php">> Referanslar</a></li>
+                </ul>
+                <table id="footermobile">
+                    <tr>
+                        <td><a href="hakkimizda.php">> Hakkımızda</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="hizmetler.php">> Hizmetler</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="urunler.php">> Ürünler</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="download.php">> İndirmeler</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="iletisim.php">> İletişim</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="referanslar.php">> Referanslar</a></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="sec contactsec">
+                <h2>Bize Ulaşın</h2>
+                <ul class="info">
+                    <li>
+                    <span><i class="material-icons" style="font-size:25px;">location_on</i> </span>
+                    <span>Antakya, Odabaşı, 31001 Antakya/Hatay</span>
+                    </li>
+                    <li>
+                    <span><i class="material-icons" style="font-size:24px">call</i> </span>
+                    <p><a href="tel:1234567890">+90(544) 649 11 42</a> <br><a href="tel:1234567890">+90(552) 528 11 42</a></p>
+                    </li>
+                    <li>
+                    <span><i class="material-icons" style="font-size:24px">forward_to_inbox</i> </span>
+                    <p><a href="mailto:yazilim@vivabilgisayar.net">yazilim@vivabilgisayar.net</a></p>
+                    </li>
+                </ul>
+                <table id="footermobile">
+                    <tr>
+                        <td><p><i class="material-icons" style="font-size:25px;">location_on</i></p></td>
+                        <td><p>Antakya, Odabaşı, 31001 Antakya/Hatay</p></td>
+                    </tr>
+                    <tr>
+                        <td><p><i class="material-icons" style="font-size:24px">call</i></p></td>
+                        <td><a href="tel:1234567890">+90(544) 649 11 42</a> <br><a href="tel:1234567890">+90(552) 528 11 42</a></td>
+                    </tr>
+                    <tr>
+                        <td><p><i class="material-icons" style="font-size:24px">forward_to_inbox</i></p></td>
+                        <td><a href="mailto:yazilim@vivabilgisayar.net">yazilim@vivabilgisayar.net</a></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+</footer>
+<div class="copyrightText">
+    <p>Copyright <i class="fa fa-copyright"></i> HIKVISION SILVER PARTNER VİVA BİLGİSAYAR. Tüm hakları saklıdır. </p>
+</div>
+
+</body>
+</html>
