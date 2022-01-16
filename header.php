150d1cf309c51186557c72867ded3cdcaaa057dd
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:28 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style2.css">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+</head>
+<body> 
+            <!-------------------main nav--------------->
+            <nav>
+            <div class="nav1">
+                <a href="index.php"><div class="logo"><img src="images/viva.png"></div></a>
+                <label for="btn" class="icon">
+                    <span class="material-icons">dehaze</span>
+                </label>
+                <input type="checkbox" id="btn">
+                <ul>
+                    <li>
+                        <a href="index.php"><i class="fas fa-home"></i> Anasayfa</a>
+                    </li>
+                    <li>
+                        <a href="hakkimizda.php"><i class="fas fa-shield-alt"></i> Hakkımızda</a>
+                    </li>
+           
+                    <li>
+                        <label for="btn-1" class="show">Hizmetler <span class="material-icons">clear_all</span></label>
+                        <a href="hizmetler.php"><i class="fas fa-star"></i> Hizmetler</a>
+                        <input type="checkbox" id="btn-1">
+                        <ul>
+                            <li><a href="hosting.php">Hosting</a></li>
+                            <li><a href="webtasarimi.php">Web Tasarımı</a></li>
+                            <li><a href="toplusms.php">Toplu SMS</a></li>
+                            <li><a href="teknikservis.php">Teknik Servisi</a></li>
+                            <li><a href="akilliev.php">Akıllı Ev Sistemleri</a></li>
+                            <li><a href="sehir.php">Şehir Analiz Sistemleri</a></li>
+                        
+                        </ul>
+                    </li>
+                    <li>
+                    <label for="btn-2" class="show">Ürünler <span class="material-icons">clear_all</span></label>
+                        <a href="urunler.php"><i class="fas fa-shopping-basket"></i> Ürünler</a>
+                        <input type="checkbox" id="btn-2">
+                        <ul>
+                                <li><a href="urunler.php">Analog HD Kameralar</a></li>
+                                <li><a href="urunler.php">HDR DVR Kayıt Cihazları</a></li>
+                                <li><a href="urunler.php">Network IP Kameralar</a></li>
+                                <li><a href="urunler.php">Network IP Kayıt Cihazı</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="download.php"><i class="fas fa-download"></i> İndirmeler</a>
+                    </li>
+                    <li>
+                        <a href="iletisim.php"><i class="far fa-comments"></i> İletişim</a>
+                    </li>
+                    <li>
+                        <a href="referanslar.php"><i class="fas fa-users"></i> Referanslar</a>
+                    </li>
+
+
+                </ul>
+                </nav>
+            </div>
+
+</body>
+</html>
