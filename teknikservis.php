24830b3802eea95dfdf2f0bf00e637ccaeb55cbc
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:20:33 +0300

    Create teknikservis.php

---
+++ teknikservis.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style6.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body style="background:rgb(235, 235, 235);">
+<?php
+    include "header.php"
+?>
+
+<div class="counter-up4">
+    <div class="content4">
+        <div class="box4">
+            <div class="counter11">Teknik Servis Hizmeti</div><br>
+            <div class="counter12">Tamir, Yedek Parça, 2.el cihaz</div>
+        </div>
+    </div>
+</div>
+
+<?php
+    include "teknik.php"
+?>
+<!--
+
+
+<div class="section">
+        <div class="container1">
+            <div class="content-section3">
+                <div class="content5">
+                    <h5>Elektronik cihazınızı güvenle teslim edebileceğiniz firmamız dahilinde teknik servisimiz 
+                        bulunmaktadır. Viva Bilgisayar teknolojik alanda gösterdiği faaliyeti hem yazılım
+                        hem donanım kapasitesiyle desteklemektedir. Bilgisayar tamiri, kamera tamiri, kamera sistemi kurulumu,
+                        alarm sistemi kurulumu, acil durum sistemleri bakım ve onarımı firmamız tarafından gerçekleştirilir.
+                    </h5>
+                        </div>
+            </div>
+            <div class="image-section2">
+                <img src="images/teknik-servis.jpg" >
+            </div>
+            
+        </div>
+    </div>
+
+-->
+
+
+
+
+<?php
+    include "footer.php"
+?>
+</body>
+</html>
