2b73daf457bcfa58417ce971e2dd092b0a186826
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:12 +0300

    Create flipbox.php

---
+++ flipbox.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+
+</head>
+<body>
+<div class="container1">
+ <div class="rowflip" id="flip-content">
+    <p><h4>Kurumsal Web Tasarım Hizmeti Verdiğimiz Firmalar</h4><br>
+    19 yılı aşkın bir deneyim ile başta Hatay olmak üzere birçok şehirde 
+    web tasarım alanında birbirinden farklı ve güzel web projeleri gerçekleştirdik. 
+    Bu zamana kadar gerçekleştirmiş olduğumuz web sitesi tasarımlarından 
+    bir kısmını burada sizinle de paylaşmak istedik. 
+    </p><br>
+ </div>
+    <div class="rowflip">
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img  id="flip-img" src="images/referans/DefneBelediyesi.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Defne Belediyesi</h2>
+                <a href="https://www.defne.bel.tr"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/TopluSMS.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Hatay Toplu SMS</h2>
+                <a href="https://www.hataytoplusms.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/HatayHost.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Hatay Host Hizmeti</h2>
+                <a href="https://www.hatayhost.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/ZenitHome.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Zenit Home</h2>
+                <a href="https://www.zenithomedekorasyon.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="rowflip">
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/OvaBoru.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Ova Boru</h2>
+                <a href="https://www.ovaboru.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/AkkusMarket.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Akkuş Market</h2>
+                <a href="https://www.akkusmarket.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/1001BalonFestivali.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>1001 Balon Festivali</h2>
+                <a href="https://www.1001balonfestivali.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/FarukYesiloglu.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Faruk Yeşiloğlu</h2>
+                <a href="https://www.farukyesiloglu.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/elfinmozaik.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Elfin Mozaik </h2>
+                <a href="https://www.elfinmozaik.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/hatayfidancilik.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Hatay Fidancılık E-Ticaret</h2>
+                <a href="https://www.hatayfidancilik.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/livakonut.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Liva Konut İnşaat</h2>
+                <a href=" https://www.livakonut.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/missgozde.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Miss Gözde Gelinlik & Abiye </h2>
+                <a href=" https://www.missgozde.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/muhsinayranci.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br> MD Ayrancı Polikliniği </h2>
+                <a href="https://www.muhsinayranci.com.tr"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/nimetkultekin.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Diyetisyen Nimet Kültekin</h2>
+                <a href=" https://www.nimetkultekin.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/oriderya.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Derya Yoğurucu</h2>
+                <a href=" https://www.oriderya.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/orifleymyesra.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Orifleymci Yesra</h2>
+                <a href="https://www.orifleymciyesra.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/orikatalogdunyasi.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Oriflame Katalog Dünyası</h2>
+                <a href="https://www.orikatalogdunyasi.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/vipwash.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Vipwash Hatay</h2>
+                <a href="https://www.vipwashhatay.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/zerifbilgin.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Oriflame Altın Direktörü Zerif Bilgin</h2>
+                <a href="https://www.zerifbilgin.com"><br>Link</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="rowflip" id="flip-content">
+    <p><h4> Farklı Kurumsal Tasarımlarımız</h4><br>
+    19 yılı aşkın bir deneyim ile başta Hatay olmak üzere birçok şehirde 
+    web tasarım alanında birbirinden farklı ve güzel web projeleri gerçekleştirdik. 
+    Bu zamana kadar gerçekleştirmiş olduğumuz web site tasarımı dışında yaptığımız logo ve kartvizit çalışmalarını
+    burada sizinle de paylaşmak istedik. 
+    </p><br>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/diger3.jpg" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Firdevs Renovasyon Kartvizit</h2>
+                
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/diger4.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Kartvizit</h2>
+                
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/diger5.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Kartvizit</h2></h2>
+                
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/diger6.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Kartvizit</h2>
+                
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/diger7.png" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Kartvizit</h2>
+                
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/diger8.jpg" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Kartvizit</h2>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/diger9.jpg" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Magnet</h2>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/diger1.jpg" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Logo</h2>
+            </div>
+        </div>
+    </div>
+    <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+                <img id="flip-img" src="images/referans/diger2.jpg" alt="" >
+            </div>
+            <div class="flip-box-back">
+                <h2><br>Logo</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
