b4e3e1551bbba509b1d7b1a5a9de93fe44b95388
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:25 +0300

    Create hakkmzda.php

---
+++ hakkmzda.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style5.css">
+
+
+</head>
+<body>
+    <div class="about-section">
+        <div class="inner-container-about">
+            <h1>
+                Hakkımızda
+                <div class="border"></div>
+            </h1>
+            <p class="text-about">
+            Haikon - Hikvision Hatay Silver Partner / Viva Bilgisayar, Sektörün en büyük Ar & Ge gücüne 
+            sahip olan Hikvision, ses ve görüntü kodlama, video görüntüsü işleme ve veri depolama gibi kilit 
+            teknolojilerin yanı sıra bulut bilişim, büyük veri ve derin öğrenme gibi geleceğin parlak teknolojilerini 
+            üreten firmanın Hikvision Hatay Silver Partneridir. Viva Bilgisayar 03.06.2000 tarihinde ilk faaliyetine 
+            başlamıştır. 21 yıla aşkın sürede bir sürü projeye imza atmış, Hatay'ın tüm ilçeleri yanı sıra, Türkiye'nin 
+            bir çok iline de hizmet vermektedir. Hikvision yenilikçi güvenlik kamerası ürün ve çözümlerinin dünya lideri 
+            tedarikçisidir ve ilerlemeye devam etmektedir. Güvenlik kamerası sektörünün yanı sıra akıllı ev teknolojileri, 
+            endüstriyel otomasyon ve otomotiv elektroniği sektörlerine de uzanan Hikvision, uzun-vadeli vizyonunu karşılamayı 
+            hedefliyor. Her zaman müşterileri için değer yaratan Hikvision, tüm dünyada varlığını göstermek adına 33 adet bölge 
+            şubesiyle hizmet veriyor.
+            </p>
+            <button>Daha Fazlası</button>
+        </div>
+    </div>
+</body>
+</html>
