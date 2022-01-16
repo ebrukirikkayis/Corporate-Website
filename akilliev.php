76f92243dc4febf4a9f81df07807b9683def2ba3
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:18:54 +0300

    Create akilliev.php

---
+++ akilliev.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style6.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body style="background:rgb(235, 235, 235);">
+<?php
+    include "header.php"
+?>
+    <div class="counter-up5">
+    <div class="content5">
+        <div class="box5">
+            <div class="counter13">Akıllı Ev Sistemleri</div><br>
+            <div class="counter14">Video gözetim sistemi, Alarm sistemleri, Giriş ve Çıkış sistemleri, Erişim kontrol sistemleri.</div>
+        </div>
+    </div>
+    </div>
+    <!---------------------------------------->
+    <?php
+        include "akillievsistemleri.php"
+    ?>
+    <!--<div class="section">
+        <div class="container1">
+            <div class="content-section">
+                <div class="content5">
+                    <h5>Şehirde yoğun bir sabah hayal edin: insanlar dairelerinden aceleyle çıkıyorlar,
+                        yemek katındaki kafede duruyorlar, metro istasyonuna koşuyorlar ve iş için tam zamanında
+                        ofislerine varıyorlar. Daireler, ticari kompleksler ve ofis binalarının tümü günlük 
+                        hayatta benzersiz amaçlara hizmet eder, ancak benzer güvenlik gereksinimlerini paylaşır.
+                        İşte tam da bu nedenle Hikvision Bina Çözümü güvenlik ve bilgi için birkaç temel bileşeni 
+                        içeren genel bir paket sunar – örneğin video gözetim sistemi, alarm sistemleri, giriş ve 
+                        çıkış sistemleri ve erişim kontrol sistemleri.
+                        <br>
+                        <br>
+                        Gerçek dünyadaki ihtiyaçlara ve sınırlamalara bağlı olarak, 
+                        her sistemin benzersiz kombinasyonları ve işlevleri özelleştirilebilir. 
+                        Tekliflerimiz, en genel güvenlik ihtiyaçlarından yönetim verimliliğini ve 
+                        kullanıcı deneyimini iyileştiren gelişmiş akıllı işlevlere kadar uzanmaktadır.</h5>
+                        <div class="button5">
+                            <a href="urunler.php">Göz At</a>
+                        </div>
+                </div>
+            </div>
+            <div class="image-section">
+                <img src="images/hizmetler/smart.png">
+            </div>
+            
+        </div>
+    </div> 
+    -->
+
+
+
+<?php
+    include "footer.php"
+?>
+</body>
+</html>
