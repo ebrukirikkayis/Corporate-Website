7655fb222023ff3b0e7827718e3f1ac28f036c2b
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:19:00 +0300

    Create bayilik.php

---
+++ bayilik.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style8.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js"></script>
+</head>
+<body style="background:rgb(235, 235, 235)">
+    
+    <div class="cont-bayi">
+        <br>
+        <h2>Yeni Bayi Başvurusu</h2>
+        <h4>Sizi Aramızda Görmekten Mutluluk Duyarız!</h4>
+        <h6>Bu sayfada bulunan formu doldurarak avantajlı bir 
+            alışveriş için ilk adımı atmış olacaksınız. Formu eksiksiz doldurmanız başvurunuzun hızlı 
+            bir şekilde ve olumlu sonuçlanması için son derece önemlidir. Başvurunuz tamamlandığında müşteri 
+            temsilcilerimiz en kısa sürede sizinle iletişime geçecektir.</h6><br>
+        
+        <h3>Firma Tanıtımı</h3>
+        <div class="firma">
+            <div class="col-fir">
+            Firma Ünvanı *<input type="text" placeholder="Firma Ünvanı"><br>
+            Vergi No *<input type="text" placeholder="Vergi No"><br>
+            </div>
+            <div class="col-fir">
+            Vergi Dairesi *<input type="text" placeholder="Vergi Dairesi"><br>
+            Kuruluş Yılı *<input type="text" placeholder="Kuruluş Yılı"><br>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    
+</body>
+</html>
