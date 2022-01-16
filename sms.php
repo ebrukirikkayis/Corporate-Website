0ab3af7145ae3599b8c56999c1f7d37296353b95
ebrukirikkayis <64480023+ebrukirikkayis@users.noreply.github.com>
2022-01-17 00:20:02 +0300

    Create sms.php

---
+++ sms.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>VİVA BİLGİSAYAR</title>
+    <link rel="stylesheet" href="style7.css">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+</head>
+<body style="background:rgb(235, 235, 235);">
+    <div class="container-sms">
+        <p id="span-sms"><a href="https://www.hataytoplusms.com/">Hatay Toplu SMS</a>, kurumlar için geleneksel iletişim yöntemleriyle karşılaştırıldığında hızlı,
+        ekonomik, etkili ve güvenilir bir iletişim ortamı sağlamakta. Ticari İşletmeler için Toplu SMS; müşterilerinize hızlı bilgilendirme mesajı geçmenizi ve 
+        ürünlerinizin kampanya, duyuru ve tanıtımını en güvenilir biçimde ve doğrudan hedef kitleye yapmanızı sağlar.
+        </p>
+
+        <p id="span-sms">
+        Viva Bilgisayar bünyesindeki <a href="https://www.hataytoplusms.com/">Hatay Toplu SMS</a>, Kurumsal SMS platformu ile hızlı, kolay, etkili, güvenli ve işinize değer 
+        yaratacak kurumsal SMS iletişim çözümleri sunuyoruz. Tüm kurumsal SMS ihtiyaçlarınızı en kolay, en hızlı ve en güvenli şekilde karşılıyor, 
+        size uçtan uca ve entegre çözümler sunuyoruz. GSM şirketlerinin, 1 hafta ya da 1 ay süreyle geçerli olan ve dönem sonunda kullanmadığınız SMS’lerin silindiği SMS paketlerinden 
+        sıkıldınız mı? Sadece tek yöne değil her yöne dilediğiniz şekilde SMS gönderebildiğiniz ve dönem sonunda silinmeyen SMS paketlerimi kullanmak istiyorsunuz? 
+        Üstelik bunu en ekonomik fiyatlarla yapmak için doğru yerdesiniz. Dilerseniz, SMS’lerinize “Originatör” tanımı vererek “başlıklı” ya da “isimli” tabir edilen SMS’ler 
+        de gönderebilirsiniz.
+        </p>
+            <div class="container-sms-sec">
+                <div class="col-span-4" >
+                    <p id="span-sms1"><i class="fas fa-sms"></i> Toplu SMS</p>
+                    <p id="span-sms2">Hatay Toplu SMS, SMS ve ses uygulamaları inşa edebilmeniz için standart web dili kullanmanıza izin verir. Dünyadaki tüm operatörlere bağlanmanızı sağlar 
+                    ve bunları temiz, güçlü bir API üzerinden sunar.</p>
+            
+                    <p id="span-sms1"><i class="fas fa-mobile-alt"></i> Mobil Ödeme</p>
+                    <p id="span-sms2">Müşterilerinize alternatif ödeme yöntemi sunmak ister misiniz? Cep telefonlarından gönderecekleri bir mesaj ile ödemenizi anında alabilir, bunu periyodik olarak 
+                    da yapabilirsiniz. Ödeme almak artık çok basit ve hızlı…</p>
+                </div>
+
+                <div class="col-span-4">
+                
+                    <p id="span-sms1"><i class="fas fa-voicemail"></i> Sesli Mesaj</p>
+                    <p id="span-sms2">Kampanyalarınız, duyurularınız, ürünleriniz ses ile hayat buluyor… Şimdi sesin etkisini müşterilerinize/kullanıcılarınıza yansıtın. Ses ile iletişim engellerini 
+                    ortadan kaldırın. Etkili iletişim için Ses’i kullanın.</p>
+
+                    <p id="span-sms1"><i class="fas fa-phone-volume"></i> Telefon</p>
+                    <p id="span-sms2">İşletmenize ait sabit  telefon hattı olması önemlidir. Bu hattın gelişmiş özelliklere sahip olması ise Hatay Toplu SMS ile mümkündür. Viva Bilgisayar standart sabit 
+                    hatlardan aldığınız hizmetin çok ötesinde bir telefon hizmeti sunuyor.</p>
+                </div>
+
+                <div class="col-span-4">
+                    <p id="span-sms1"><i class="fas fa-mail-bulk"></i> Toplu MMS</p>
+                    <p id="span-sms2">Artık sizi hikayenin tamamını göndermekten alıkoyan iletişim engellerini ortadan kaldırabilirsiniz. Resim gönderme yeteneği ile beraber mesajlarınız öncekillerden daha isabetli, 
+                    daha canlı ve her zamankinden daha ilham verici olmasını sağlıyoruz.</p>
+
+                    <p id="span-sms1"><i class="fas fa-fax"></i> Faks</p>
+                    <p id="span-sms2">Hatay Toplu SMS ile dünyanın her yerine artık kolayca faks gönderebilir veya istediğiniz her yerden faksınızı alabilirsiniz. Bunun için artık bir faks cihazına ihtiyacınız yok. 
+                    Tek tuşla aynı anda yüzlerce faksı gönderebilir, gelen fakslarınızı emailinizden okuyabilirsiniz.</p>
+                </div>
+            </div>
+
+            <div class="container-sms-third">
+                <div class="col-span-12" id="span-12">
+                    <img id="span-third-img" src="images/toplusms/akilliisletme.jpg">
+                    <div class="col-span-8" id="span-8">
+                    <p id="span-sms3"><b>Akıllı İletişimler İnşa Edin</b></p>
+                    <p id="span-sms2">Hatay Toplu SMS, SMS ve ses uygulamaları inşa edebilmeniz için standart web dili kullanmanıza izin verir. Dünyadaki tüm operatörlere bağlanmanızı sağlar ve bunları temiz, 
+                            güçlü bir API üzerinden sunar. O yüzden favori programlama dilinizi ve bir web barındırma hizmeti alıp gelin ve bizimle birlikte iletişimin gelecek neslini inşa edin.
+                    </p>
+                </div>
+                </div>
+                
+                <div class="col-span-12" id="span-12">
+                    <div class="col-span-8" id="span-8">
+                    <p id="span-sms3"><b>Bulut ile daha güçlü!</b></p>
+                    <p id="span-sms2">Biz bulut sistemler üzerinde inşa edildik. API’mız her zaman çalışır, sürekli güncellenir ve sizin ihtiyaçlarınıza göre otomatik olarak ölçeklenir. İletişimlerinizi bulut 
+                            üzerine taşıdığınızda, ortada ayarlanacak hiçbir çetrefilli VPN veya yönetilecek hiçbir SMPP bağlantısı kalmaz. Sadece mesajınızı bize HTTP üzerinden gönderin ve biz de bunu dünyada 
+                            herhangi bir yere iletelim.
+                    </p>
+                    </div>
+                    <img id="span-third-img"  src="images/toplusms/bulut.jpg">
+                </div>
+            </div>
+
+            <div class="col-span-12" id="colheader2">
+                <p id="span-sms6">SMS ve Uygulamalarımız</p>
+                <p id="span-sp3">Birbirleri için yaratılmışlar!</p>
+            </div>
+            <div class="container-fourth">
+                <div class="col-span-6">
+                    <img id="span-fourth-img" src="images/toplusms/musteri.jpg">
+                    <p id="span-sms4"><b>Müşteri Bildirimleri</b></p>
+                    <p id="span-sms5">Müşterilerinize masalarının hazır olduğunu, paketlerinin teslim edildiğini veya faturalarının vadesinin geldiğini söyleyin. Bir SMS gönderdiğinizde, onu anında 
+                        kullanıcılarınızın mobil telefonuna teslim edilmesini sağlarız.
+                    </p>
+
+                    <img id="span-fourth-img" src="images/toplusms/hesapdogrulama.jpg">
+                    <p id="span-sms4"><b>Hesap Doğrulama</b></p>
+                    <p id="span-sms5">Kaydolma aşamasında bir hesaba bir gönderen başlığı atayın. Her kullanıcının benzersiz bir telefon numarası olduğunu kontrol ederek, mükerrer hesapları elemiş olursunuz. 
+                        SMS, geniş ölçekte müşterilerinizi doğrulama açısından düşük maliyetli bir yoldur.
+                    </p>
+                </div>
+
+                <div class="col-span-6">
+                    <img id="span-fourth-img" src="images/toplusms/grupmesajlasma.jpg">
+                    <p id="span-sms4"><b>Grup Mesajlaşma</b></p>
+                    <p id="span-sms5">Yalnızca bir telefon numarası kullanarak SMS üzerinden iletişimi sağlarız. Grup mesajlaşma ile öğretmenler öğrencilerine bir ödev hakkında bilgi 
+                        verebilir veya iş aradaşları herkesin yeni teklifi görmesini sağlayabilir.
+                    </p>
+
+                    <img id="span-fourth-img" src="images/toplusms/kullaniciguvenligi.png">
+                    <p id="span-sms4"><b>Kullanıcı Güvenliği</b></p>
+                    <p id="span-sms5">Bu donanım fobisini unutun. Kullanıcıların telefonlarına tek seferlik geçici SMS şifreleri göndererek websitesi girişlerine ek bir güvenlik katmanı ekleyin. Bu ekstra adım, 
+                        sitenize giriş yapan kullanıcının sizin istediğiniz olduğunu garanti eder.
+                    </p>
+                </div>
+
+                
+            </div>
+        
+
+    </div>
+</body>
+</html>
